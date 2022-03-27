0410c1adcf054c3d2725fbc6b556c7a3d1f338f0
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2022-03-27 00:14:22 -0300

    Update weather-observation-station-9.sql

--- weather-observation-station-9.sql
+++ weather-observation-station-9.sql
@@ -1,4 +1,4 @@
-link: https://www.hackerrank.com/challenges/weather-observation-station-9/problem?isFullScreen=true
+--link: https://www.hackerrank.com/challenges/weather-observation-station-9/problem?isFullScreen=true
 
 SELECT distinct City 
 FROM STATION 
