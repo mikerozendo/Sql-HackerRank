c0476abe8619a8527235daf08d66e6e4302de491
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2022-03-27 00:05:55 -0300

    Create the-pads.sql

---
+++ the-pads.sql
@@ -0,0 +1,20 @@
+https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
+
+select 
+occupation as 'occupation', 
+count(occupation) as 'occupation_count'
+into #t_occupations_count
+from occupations 
+group by occupation
+
+select 
+concat(name,'(',substring(occupation,1,1),')')
+from occupations
+order by name
+
+select
+concat('There are a total of ',occupation_count, ' ',lower(occupation),'s.')
+from #t_occupations_count
+order by occupation_count
+
+drop table #t_occupations_count
