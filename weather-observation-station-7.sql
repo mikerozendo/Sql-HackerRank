90014b6ef92f91d75878a11e18bd3b4f7019e973
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2022-03-27 00:18:36 -0300

    Update and rename Sql-HackerRank-Easy/weather-observation-station-7 to weather-observation-station-7.sql

--- weather-observation-station-7.sql
+++ weather-observation-station-7.sql
@@ -1,4 +1,4 @@
-link: https://www.hackerrank.com/challenges/weather-observation-station-7/problem?isFullScreen=true
+--link: https://www.hackerrank.com/challenges/weather-observation-station-7/problem?isFullScreen=true
 
 SELECT DISTINCT CITY FROM STATION 
 WHERE CITY LIKE '%a'
