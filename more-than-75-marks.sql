20fb48a3aafae7744da3c2d7d9908dbbf0a3db69
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2022-03-28 21:02:08 -0300

    Update more-than-75-marks.sql

--- more-than-75-marks.sql
+++ more-than-75-marks.sql
@@ -1,16 +1,7 @@
 --link:https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true
 
---#TODO ************8
-
-SELECT STUDENTS.Name as Name, STUDENTS.ID as Id INTO #t_names 
-FROM STUDENTS WHERE Marks > 75 
-ORDER BY SUBSTRING(Name, LEN(Name) - 3, LEN(Name))
-
-
-CASE WHEN
-(SELECT COUNT(*) FROM #t_names
-GROUP BY SUBSTRING(Name, LEN(Name) - 3, LEN(Name)))
-THEN (SELECT Name FROM #t_names
-ORDER BY SUBSTRING(Name, LEN(Name) - 3, LEN(Name)), Id)
-ELSE
-    (SELECT Name FROM #t_names) END
+select 
+name 
+from students
+where marks > 75
+order by right(name,3), id
