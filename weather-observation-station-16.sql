75c1891cdd70f382e1f9d8f661cebf7f1aa401f0
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2022-03-28 21:52:21 -0300

    Update weather-observation-station-16.sql

--- weather-observation-station-16.sql
+++ weather-observation-station-16.sql
@@ -1,4 +1,4 @@
---https://www.hackerrank.com/challenges/weather-observation-station-16/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+--https://www.hackerrank.com/challenges/weather-observation-station-16/problem?isFullScreen=true
 
 select 
 cast(min(lat_n) as decimal(36,4))
